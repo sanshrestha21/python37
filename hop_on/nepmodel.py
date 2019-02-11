57463ffe0b03a54d78188602c4161cd8bdaf5a87
san <sanshrestha21@gmail.com>
2019-02-11 16:39:33 +0545

    hop folder reinvented

--- hop_on/nepmodel.py
+++ hop_on/nepmodel.py
@@ -13,54 +13,23 @@ bs = BeautifulSoup(html, 'html.parser')
 
 images = bs.find_all('img', {'src':re.compile('.jpg')})
 
-
-#for image in images: 
-
 for image in images:
-    #print(image['src']+'\n')
+  
     model_image = [image['src']]
-    #model_image=model_image.append(model_image)
+    
     #print(model_image)
     ekta_img_url = [i for i in model_image if re.search('ekta', str(i))]
     #print(ekta_img_url)
-#print("#############")
+
     for data in ekta_img_url:
         imgurl="http://cybersansar.com/"+data+""
         imgurl=imgurl.replace("/thumb","")
-        #print(imgurl)
+       
+        if not os.path.exists('Ekta'):
+            os.makedirs('Ekta')
+           
+        image_test=requests.get(imgurl,stream=True)
+        tokens=imgurl.split('/')
 
-
-        tokens=imgurl.split("/")
-        file_name=tokens[len(tokens)-1]
-        file=open(file_name,"w+")
-        file.close()
-# if not os.path.exists('Ekta'):
-#     os.makedirs('Ekta')
-
-# for i in data:
-#     with open('Ekta/' + os.path.split(i)[1], 'wb') as f:
-#         print(i)
-#         f.write(requests.get(i).content)
-
-    #i=[]
-#print("############")
-
-# for i in model_image:
-#     #i=[re.search('ekta', model_image,"wb")]
-#     i = [re.search(r'\bekta_kunwar\w+', i)]
-# #     #i.append
-#     print(i)
-#print(['Ekta_img_url'] +'\n')
-
-#bs = BeautifulSoup(html, 'html.parser')
-#images = bs.find_all('img', {'src':re.compile('.jpg')})
-# for image in images: 
-#     print(image['src']+'\n')
-
-# if not os.path.exists('Ekta'):
-#     os.makedirs('Ekta')
-
-# for i in Ekta_img_url:
-#     with open('Ekta/' + os.path.split(i)[1], 'wb') as f:
-#         print(i)
-#         f.write(requests.get(i).content)
+        with open('Ekta/' + tokens[len(tokens)-1], 'wb') as f:
+            f.write(image_test.content)
